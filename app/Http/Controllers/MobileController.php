bd9715eb473ec4e6fc58b94720c2173215985383
ouss <mahgounoussama545@yahoo.fr>
2019-05-29 17:16:23 +0100

    units account

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -8,9 +8,13 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
-
+use Hash;
 class MobileController extends Controller
 {
+    public function getrandomkey(Request $request){
+       
+        return response()->json(Str::random(60));
+    }
 
     //use this only once
     public function initKeys(){
@@ -26,17 +30,51 @@ class MobileController extends Controller
     public function login(Request $request){
         $success = true;
         $api_token = '';
+        $email = request('email');
+        $password = request('password');
 		$user=null;
-        if (Auth::attempt(['email' => request('email'), 'password' => request('password')])){
+        if (Auth::attempt(['email' => $email, 'password' => $password])){
             $user = Auth::user();
             $api_token = $user->api_token;
 			$role = Auth::user()->captain->assignedRole->getRole();
 			$user = User::with(["profile","profile"])->where('scout_id',$user->scout_id)->get()->first();
         }else{
-            $success = 'false';
+            if($email == config('unitAccount.units.cubs.email') ||
+            $email == config('unitAccount.units.scouts.email') ||
+            $email == config('unitAccount.units.advancedscouts.email') ||
+            $email == config('unitAccount.units.travelers.email') 
+            ){
+               
+                if($email == config('unitAccount.units.cubs.email') && Hash::check($password,config('unitAccount.units.cubs.password'))){
+                    $api_token = config('unitAccount.units.cubs.api_token');
+
+                    return response()->json(['success' => $success,'api_token' =>$api_token,"unit_name"=>config('unitAccount.units.cubs.unit_name')]);
+                }
+                if($email == config('unitAccount.units.scouts.email') && Hash::check($password,config('unitAccount.units.scouts.password'))){
+
+                   $api_token = config('unitAccount.units.scouts.api_token');
+
+                    return response()->json(['success' => $success,'api_token' =>$api_token,"unit_name"=>config('unitAccount.units.scouts.unit_name')]);
+
+                }
+                if($email == config('unitAccount.units.advancedscouts.email')  && Hash::check($password,config('unitAccount.units.advancedscouts.password'))){
+                    $api_token = config('unitAccount.units.advancedscouts.api_token');
+                    return response()->json(['success' => $success,'api_token' =>$api_token,"unit_name"=>config('unitAccount.units.advancedscouts.unit_name')]);
+
+                }
+                if($email == config('unitAccount.units.travelers.email') && Hash::check($password,config('unitAccount.units.travelers.password'))){
+
+                    $api_token = config('unitAccount.units.travelers.api_token');
+                    return response()->json(['success' => $success,'api_token' =>$api_token,"unit_name"=>config('unitAccount.units.travelers.unit_name')]);
+
+                }
+            }else
+              
+             $success = false;
         }
-        return response()->json(['success' => $success,'api_token' =>$api_token,"first_name"=>$user->profile->first_name,"last_name"=>$user->profile->last_name,"scout_id"=>$user->scout_id,"image"=>$user->profile->image,"role"=>
+      return response()->json(['success' => $success,'api_token' =>$api_token,"first_name"=>$user->profile->first_name,"last_name"=>$user->profile->last_name,"scout_id"=>$user->scout_id,"image"=>$user->profile->image,"role"=>
 $role]);
+
     }
 
     //return the loged in user
---
+++ config/unitAccount.php
@@ -0,0 +1,43 @@
+
+<?php
+return [
+"units"=>[
+"cubs"=>[
+    'unit_name'=>"الأشبال",
+    'email'=>"cubs@falehscout.com",
+    'password'=>'$2a$10$Q8aAk2Pp2EBMlHTPp5CXQu55bXVmKisUeisMYVQZu2X/HtiM8xjVO', // Cubsunit
+    'api_token'=>'uUP2lfSe1re4A5LRzIY9A4V8S1DRjvYTF8X9Y1a09OBiKph4wIPkav3tw83d',
+],
+
+"scouts"=>[
+    'unit_name'=>"الكشاف",
+'email'=>"cubs@falehscout.com",
+'password'=>'$2a$10$GLlD8YkRW/z/0vue6pMpH.Db3Dnk6ShOyvDWVSNl8LcpYmXr3fFUe', // Scoutsunit
+'api_token'=>'zCLdaGx7wr7ta3tYJxOetR9MO1pR8ySzT1Jrjj5fEXrU5tWm4PeIgM5XnTsx',
+],
+"advancedscouts"=>[
+    'unit_name'=>"الكشاف المتقدم",
+'email'=>"cubs@falehscout.com",
+'password'=>'$2a$10$RrhzFflBH3xMwAEoJ2QEpesNtR0Py6vFCXpAPU8sVogri6hB4prrG', // AdScoutsunit
+'api_token'=>'eRggfgm3gRyZNt4f6Oum0bjQKbPZI1AAl4RIL306vLPJEBgK9VWDkOTWrcN4',
+],
+"travelers"=>[
+    'unit_name'=>"الجوالة",
+'email'=>"cubs@falehscout.com",
+'password'=>'$2a$10$k6cXcYfDgQ0iwI99nlt2PuBgmuWurRfVczDVXGfM0jS.VjwgqNA1C', // Travelerunit
+'api_token'=>'YC0OeRre2uRzaDPXHayVH7yn1nYKR7I0p0ZNGAXpAEzaHwWTwjmFuP4uuBxV'
+],
+
+
+
+
+
+
+
+
+
+
+
+
+],
+];
